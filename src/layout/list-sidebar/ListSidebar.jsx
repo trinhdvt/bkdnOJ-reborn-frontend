15f93fd945a6bc66cec0f4f4b2070ec89ac03baf
nvatuan <24392632+nvatuan@users.noreply.github.com>
2022-05-10 19:02:35 +0700

    Optimizing bootstrap import

--- src/layout/list-sidebar/ListSidebar.jsx
+++ src/layout/list-sidebar/ListSidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 import { log } from 'helpers/logger';
 
 import './ListSidebar.scss';
@@ -7,22 +7,60 @@ import OutsideAlerter from 'helpers/OutsiderAlerter';
 
 class Offcanvas extends React.Component {
   render() {
+    const sideComponents = this.props.sideComponents;
+
     return (
       <div id="offcanvas" className="offcanvas">
-        <a className="canvasCloseBtn" onClick={() => this.props.closeCanvas()}>
-          ⭲
-        </a>
+        {
+          sideComponents.map( (comp, indx) => (
+            <div key={`side-component-${indx}`} 
+              className="side-component rounded">
+              {comp}
+            </div>
+          ))  
+        }
 
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Clients</a>
-        <a href="#">Contact</a>
-
+        <div className="offcanvasCloseBtn-wrapper">
+          <Button id="offcanvasCloseBtn" className="offcanvasCloseBtn btn-dark hidden" onClick={() => this.props.closeCanvas()}>
+            ⭲
+          </Button>
+        </div>
       </div>
     )
   }
 }
 
+class MainContent extends React.Component {
+  render() {
+    const children = this.props.children || (
+      <>
+        <h2>Offcanvas Push Example</h2>
+        <p>Please pass a React component to `children` in order to replace this message.</p>
+      </>
+    )
+
+    return (
+      <div className="offcanvas-menu border border-primary" id="offcanvas-menu">
+        { children }
+        <div className="offcanvasOpenBtn-wrapper">
+          <Button className="offcanvasOpenBtn btn-dark" onClick={() => this.props.toggle()}>
+            ⭰
+          </Button>
+        </div>
+      </div>
+    )
+  }
+}
+
+/*
+ * A better name would be 8-4 layout. This layout will display
+   main content by 8 | side content 4 on wide screen. On mobile,
+   side content will be put onto an off-canvas, that can be accessed
+   by a fixed button on the bottom
+ * params:
+     - mainContent: a React Component, will appear in the main content section
+     - sideComponents: a list of React Components, appear on the side or off-canvas
+ */
 export default class ListSidebar extends React.Component {
   constructor(props) {
     super(props);
@@ -34,39 +72,48 @@ export default class ListSidebar extends React.Component {
   }
 
   render() {
-    log(this.state.offcanvasShow)
-    const controller = this;
+    const mainContent = this.props.mainContent || (<p>This is main content</p>);
+    const sideComponents = this.props.sideComponents || [
+      (<p> Component 1 </p>),
+      (<p> Component 2 </p>),
+      (<p> Component 3 </p>),
+    ]
+
     return (
-      <>
-        <OutsideAlerter 
-          children={ <Offcanvas closeCanvas={() => this.close()}/> }
-          isDetecting={this.state.offcanvasShow}
-          outsideClickHandler={() => this.close()}
-        />
-        <div id="offcanvas-menu">
-          <h2>Sidenav Push Example</h2>
-          <p>Click on the element below to open the side navigation menu, and push this content to the right.</p>
-          <span style={{fontSize: "30px", cursor: "pointer"}} onClick={() => this.toggle()}>&#9776; open</span>
-        </div>
-      </>
-      // <Row>
-      //   <Col md={8} className='table-col center border border-primary'>
-      //     <div className="list">
-      //       abcdef
-      //     </div>
-      //   </Col>
-      //   <Col md={4} className='comp-col center border border-warning d-none d-md-block'>
-      //     <div className="component-1 border border-danger">
-      //       xxxx
-      //     </div>
-      //     <div className="component-2 border border-danger">
-      //       xxxx
-      //     </div>
-      //     <div className="component-3 border border-danger">
-      //       xxxx
-      //     </div>
-      //   </Col> 
-      //  </Row> 
+      <div className="list-sidebar-wrapper">
+        <Row className="d-block d-md-none row">
+          <OutsideAlerter 
+            children={ 
+              <Offcanvas 
+                closeCanvas={() => this.close()}
+                sideComponents={sideComponents}
+              /> 
+            }
+            isDetecting={this.state.offcanvasShow}
+            outsideClickHandler={() => this.close()}
+          />
+
+          <MainContent toggle={() => this.toggle()}
+            children={mainContent}
+          />
+
+        </Row>
+
+        <Row className="d-none d-md-flex row">
+          <Col md={8} className='table-col center border border-primary'>
+            { mainContent }
+          </Col>
+          <Col md={4} className="comp-col center border border-warning">
+            {
+              sideComponents.map( (comp, indx) => (
+                <div key={`side-component-${indx}`} className="border border-danger">
+                  {comp}
+                </div>
+              ))  
+            }
+          </Col> 
+        </Row> 
+      </div>
     )
   }
 
@@ -75,22 +122,21 @@ export default class ListSidebar extends React.Component {
     if (bool) this.close();
     else this.open();
   }
-  setShow(bool) {
-    this.setState({offcanvasShow: bool})
-  }
 
   open() {
-    const sidenav = document.getElementById("offcanvas")
-    if (sidenav) sidenav.style.width = "250px";
-    const main = document.getElementById("offcanvas-menu")
-    if (main) main.style.marginRight = "250px";
-    this.setShow(true)
+    this.setState({offcanvasShow: true}, () => {
+      const sidenav = document.getElementById("offcanvas")
+      if (sidenav) sidenav.style.width = "350px";
+      // const main = document.getElementById("offcanvas-menu")
+      // if (main) main.style.marginRight = "250px";
+    })
   }
   close() {
-    const sidenav = document.getElementById("offcanvas")
-    if (sidenav) sidenav.style.width = "0";
-    const main = document.getElementById("offcanvas-menu")
-    if (main) main.style.marginRight = "0";
-    this.setShow(false)
+    this.setState({offcanvasShow: false}, () => {
+      const sidenav = document.getElementById("offcanvas")
+      if (sidenav) sidenav.style.width = "0";
+      // const main = document.getElementById("offcanvas-menu")
+      // if (main) main.style.marginRight = "0";
+    })
   }
 };
